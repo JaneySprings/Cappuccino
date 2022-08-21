d4a0f2eb38405e68d5add96ad0e2163ec36ee79b
nromanov <alis228300@yandex.ru>
2022-08-21 17:34:35 +0400

    Port Auth to code

--- Cappuccino.App.iOS/UI/Auth/AuthViewController.cs
+++ Cappuccino.App.iOS/UI/Auth/AuthViewController.cs
@@ -8,36 +8,22 @@ namespace Cappuccino.App.iOS.UI.Auth {
     public partial class AuthViewController : UIViewController {
         private readonly AuthManager authManager = new AuthManager();
 
-        public AuthViewController() : base("AuthViewController", null) {}
-        public AuthViewController(IntPtr handle) : base(handle) { }
-
-        public override void ViewDidLoad() {
-            base.ViewDidLoad();
-            SetNeedsStatusBarAppearanceUpdate();
+        public override void ViewDidAppear(bool animated) {
+            base.ViewDidAppear(animated);
 
             this.authManager.Authorized += (sender, args) => {
                 SceneDelegate? sceneDelegate = View?.Window.WindowScene?.Delegate as SceneDelegate;
                 sceneDelegate?.ChangeViewControllerForHosting();
             };
-
-            NSUrl url = NSUrl.FromString(this.authManager.BuildAuthorizationUri());
+            NSUrl url = NSUrl.FromString(this.authManager.BuildAuthorizationUri())!;
             NSUrlRequest request = NSUrlRequest.FromUrl(url);
-            webView.AddObserver(this, "URL", NSKeyValueObservingOptions.New, IntPtr.Zero);
+            webView!.AddObserver(this, "URL", NSKeyValueObservingOptions.New, IntPtr.Zero);
             webView.LoadRequest(request);
-        }
-
-        public override void DidReceiveMemoryWarning() {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
         }
 
         public override void ObserveValue(NSString keyPath, NSObject ofObject, NSDictionary change, IntPtr context) {
             if (keyPath == "URL")
-                this.authManager.TryAuthorizeFromUri(webView.Url?.ToString() ?? "");
-        }
-
-        public override UIStatusBarStyle PreferredStatusBarStyle() {
-            return UIStatusBarStyle.DarkContent;
+                this.authManager.TryAuthorizeFromUri(webView!.Url?.ToString() ?? "");
         }
     }
 }
---
+++ Cappuccino.App.iOS/UI/Auth/UIAuthViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UIKit;
+
+namespace Cappuccino.App.iOS.UI.Auth {
+    public partial class AuthViewController {
+        private WebKit.WKWebView? webView;
+
+        public AuthViewController(IntPtr handle) : base(handle) { }
+        public AuthViewController() { }
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
+
+            this.webView = new WebKit.WKWebView(this.View!.Bounds, new WebKit.WKWebViewConfiguration());
+            this.View.AddSubview(webView);
+
+            SetNeedsStatusBarAppearanceUpdate();
+        }
+
+        public override void ViewDidUnload() {
+            base.ViewDidUnload();
+
+            if (webView != null) {
+                webView.Dispose();
+                webView = null;
+            }
+        }
+
+        public override UIStatusBarStyle PreferredStatusBarStyle() {
+            return UIStatusBarStyle.DarkContent;
+        }
+    }
+}
+
